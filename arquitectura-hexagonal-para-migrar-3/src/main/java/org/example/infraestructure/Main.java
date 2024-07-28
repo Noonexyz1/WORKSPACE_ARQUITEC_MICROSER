eddc17a8e743111e68926e32b8f24b5d364a8af3
Diego Kali <aries-d2github@hotmail.com>
2024-07-28 17:36:09 -0400

    fix: arqui-hexa-para-migrar-3, Configuracion POM completa para empaquetar el jar con Spring Boot

--- arquitectura-hexagonal-para-migrar-3/src/main/java/org/example/infraestructure/Main.java
+++ arquitectura-hexagonal-para-migrar-3/src/main/java/org/example/infraestructure/Main.java
@@ -12,36 +12,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
 
-        //Implementacion del REST, si esto se ejecuta al ultimo de t odo, el codigo
-        //t odo eso se reinicia, quiza por la configuracion de los beans, pero funciona
+    public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
 
-
-        //Implementando BD
-        // Inicializa HibernateUtil
         HibernateUtil hibernateUtil = new HibernateUtil("hibernate1.cfg.xml");
-
-        //Crea una instancia de RepoHibernateMariaDB
-        //puerto de mi Application  =  Implementacion de mi Adapter Infrastructure (esto es lo unico que cambia)
-        //CAMBIANDO DE IMPLEMENTACION
         AbogadoService abogadoService = new RepoHibernateMariaDB(hibernateUtil.getSessionFactory());
         //AbogadoService abogadoService = new RepoAdapterAPIExterior();
         //AbogadoService abogadoService = new RepoInMemoryAdapter();
         //AbogadoService abogadoService = new RepoInMemoryAdapter2();
 
-        // Ejemplo de uso de saveNewAbogado
         abogadoService.saveNewAbogado(new AbogadoDTO(1 /*Este id es AUTOINCREMENT, no hace falta*/, "Diego D.", "diego@diego.com"));
 
-        // Ejemplo de uso de getAllAbogados
         System.out.println("Lista de abogados:");
         for (AbogadoDTO abogadoDTO : abogadoService.getAllAbogados()) {
             System.out.println("ID: " + abogadoDTO.getId() + ", Nombre: " + abogadoDTO.getNombre() + ", Email: " + abogadoDTO.getEmail());
         }
 
-        // Cierra la SessionFactory
         hibernateUtil.shutdown();
-
     }
 }
